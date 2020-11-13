75864855ca5c5f065fd9182a3b2b5deaff03f44f
krobus00 <michaelglaxy@gmail.com>
2020-11-13 12:23:30 +0700

    tambah tkcalendar

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import tkinter as tk
+from tkcalendar import Calendar, DateEntry
+
+def listTodo(cb):
+    print(cal.selection_get())
 
 root = tk.Tk()
 root.title("Calendar Todo")
-
+cal = Calendar(root, font="Arial 14", selectmode='day', locale='id_ID',
+                cursor="hand1")
+cal.bind( "<<CalendarSelected>>", listTodo )
+cal.grid(row=0, column=0)
 root.mainloop()
